bd94f1e691e5c0329144620a2bff4add0a11e7fa
lj240556 <1226263601@qq.com>
2019-11-16 10:17:36 +0800

    852369

--- src/util/mm.jsx
+++ src/util/mm.jsx
@@ -7,7 +7,7 @@ const conf = {
     // serverHost: 'http://admin.happymmall.com'
     // dev
     serverHost: '',
-    imageHost: 'http://l06.75.172.117:8162/img/'
+    imageHost: 'http://l06.75.172.117:81/'
 }
 
 class MMUtil{
